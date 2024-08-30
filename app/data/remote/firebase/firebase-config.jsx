2d202cd85b5a787afad7ab3547352181494b14eb
Donald lwandle <donaldlwandle@gmail.com>
2024-08-30 15:28:18 +0200

    updated the firebase config

---
+++ app/data/remote/firebase/firebase-config.jsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBK2ALL9Wd9g5YlGUZLhL5aAE5_hSuPQK0",
+    authDomain: "smart-health-ae1cb.firebaseapp.com",
+    projectId: "smart-health-ae1cb",
+    storageBucket: "smart-health-ae1cb.appspot.com",
+    messagingSenderId: "576186942286",
+    appId: "1:576186942286:web:8d12a691d62917621c941f",
+    measurementId: "G-V31RSG4KL5"
+  };
+
+export const app = initializeApp(firebaseConfig);
+
