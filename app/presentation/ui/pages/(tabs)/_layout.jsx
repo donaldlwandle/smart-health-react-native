e0b9c73ae21d32f54b011b397a01cf3a4545fb39
Donald lwandle <donaldlwandle@gmail.com>
2024-10-11 18:29:09 +0200

    modified bottom navbar/tab_bar ready to add icons and shit

--- app/presentation/ui/pages/(tabs)/_layout.jsx
+++ app/presentation/ui/pages/(tabs)/_layout.jsx
@@ -1,6 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { Tabs, Redirect } from 'expo-router'
+
+const TabIcon =({icon,color, name ,focused})=>{
+  return(
+    <View>
+      <Image 
+        source ={icon}/>
+    </View>
+  )
+}
 
 const TabsLayout = () => {
   const screenOptions = {headerShown : false}
@@ -8,7 +17,39 @@ const TabsLayout = () => {
     <>
         <Tabs screenOptions={screenOptions}>
             <Tabs.Screen 
-            name="home"
+              name="home"
+              options={{
+                title:"Home",
+                headerShown: false,
+                tabBarIcon:({color,focused})=>(
+                  <TabIcon/>
+
+                )
+              }}
+            />
+
+            <Tabs.Screen 
+              name="profile"
+              options={{
+                title:"Profile",
+                headerShown: false,
+                tabBarIcon:({color,focused})=>(
+                  <TabIcon/>
+
+                )
+              }}
+            />
+
+            <Tabs.Screen 
+              name="settings"
+              options={{
+                title:"Settings",
+                headerShown: false,
+                tabBarIcon:({color,focused})=>(
+                  <TabIcon/>
+
+                )
+              }}
             />
 
         </Tabs>
